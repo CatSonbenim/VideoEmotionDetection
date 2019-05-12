57987739cac3606055f10d652471cf95bdf53318
Lisa Bulala <47056943+CatSonbenim@users.noreply.github.com>
2019-05-12 22:54:25 +0300

    Update detector_controller.py

--- detector_controller.py
+++ detector_controller.py
@@ -3,7 +3,6 @@ import cognitive_face as CF
 from PyQt5.QtWidgets import QApplication
 from detector_view import MainWindow
 
-
 def main():
     key = '53be708c65394e50afd4688fe12b468f'
     CF.Key.set(key)
@@ -14,7 +13,6 @@ def main():
     app = QApplication(sys.argv)
     ex = MainWindow()
     sys.exit(app.exec())
-
 
 if __name__ == '__main__':
     main()
